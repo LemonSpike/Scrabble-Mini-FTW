c19eaeb819f51e286611bdf9ad25ed07866e81bd
Pranav Kasetti <pranavkasetti@gmail.com>
2021-04-11 12:46:44 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Pranav Kasetti on 11/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
